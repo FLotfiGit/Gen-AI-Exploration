529da237e419d4ffee30afebbc5f859d8e93ab35
FLotfiGit <flotfi@clemson.edu>
2025-09-16 10:43:08 -0400

    add levenshtein_distance function

--- Sample_ranking.py
+++ Sample_ranking.py
@@ -118,3 +118,20 @@ def jaccard_similarity(ticket1, ticket2):
     union = set1 | set2
     return len(intersection) / len(union) if union else 0.0
 
+def levenshtein_distance(s1, s2):
+    """Compute Levenshtein distance between two strings."""
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+    if len(s2) == 0:
+        return len(s1)
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    return previous_row[-1]
+
