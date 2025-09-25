fd59d297bc0407f09dc0b0bfbfefc792ee89e3d6
FLotfiGit <flotfi@clemson.edu>
2025-09-25 11:18:01 -0400

    up

--- Simple_retrieval.py
+++ Simple_retrieval.py
@@ -1,5 +1,6 @@
-from Coding_exercise import TFIDF
+# from coding_camp import TFIDF
 import math
+from collections import Counter
 #: Utils 
 
 # chunk each string (sliding window)
@@ -27,6 +28,45 @@ def top_cosine(query_vec, doc_vec, k):
     sims = [(i, cosine_sim_sparse(query_vec, v)) for i,v in enumerate(doc_vec)]
     sims.sort(key=lambda x:x[1], reverse=True)
     return sims[:k]
+
+################# TFIDF class
+##-- TF-IDF
+
+class TFIDF:
+    def __init__(self):
+        self.vocab = {}
+        self.idf ={}
+    
+    def fit(self,docs):
+        df = Counter()
+
+        for doc in docs:
+            tokens = doc.lower.split()
+            for t in tokens:
+                df[t] +=1
+        self.vocab = {t:i for t,i in enumerate(sorted(tokens))}
+        N = len(docs)
+        idf = {math.log((1+N)/(1+df[t])+1) for t in tokens}
+    
+    def transform(self, docs):
+        rows ={}
+
+        for doc in docs:
+            tokens = doc.lower.split()
+            tf = Counter(tokens)
+            
+            vec ={}
+            for t, cnt in tf.items():
+                i = self.vocab[t]
+                vec[i] = (cnt/len(tokens)) * self.idf[t]
+                rows.append(vec)
+        
+        return rows
+    
+    def fit_transform(self,docs):
+        self.fit(docs)
+        return self.transform(docs)
+
 
 ################ retrieval docs #################
 
