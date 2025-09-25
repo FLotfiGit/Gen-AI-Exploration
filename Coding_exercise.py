d9657fe149ac0ff7a1a74750442ee585856c312d
FLotfiGit <flotfi@clemson.edu>
2025-09-25 09:15:04 -0400

    add top_tfidf_terms_sparse function for sparse TF-IDF vectors

--- Coding_exercise.py
+++ Coding_exercise.py
@@ -137,3 +137,8 @@ def average_sparse_vector(vec):
     if not vec:
         return 0.0
     return sum(vec.values()) / len(vec)
+
+def top_tfidf_terms_sparse(vec, vocab, top_n=5):
+    """Return the top N terms with highest TF-IDF values from a sparse vector."""
+    inv_vocab = {i:t for t,i in vocab.items()}
+    return sorted([(inv_vocab[i], v) for i, v in vec.items()], key=lambda x: x[1], reverse=True)[:top_n]
