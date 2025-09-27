866108597d0271785806f7e355d8b65fab5378c0
FLotfiGit <flotfi@clemson.edu>
2025-09-27 13:36:02 -0400

    u

--- Coding_LLM_RAG_tries.py
+++ Coding_LLM_RAG_tries.py
@@ -60,6 +60,53 @@ def chunk_text(s,size = 400, overlap=100):
         i += size - overlap
     return out
 #---------------------------------------------
+# tokenizer trunk
+def tokenize(text):
+    return text.strip().split()
+
+def truncate_tokens(tokens, max_len):
+    return tokens[:max_len]
+#--------------------------------------------
+# retrieval: sparse TF-IDF + cosine 
+from collections import Counter
+
+class TFIDF:
+    def fit(self,docs):
+
+        df = Counter()
+        for d in docs:
+            toks = d.lowe().split()
+            for t in toks: df[t] +=1
+        self.vocab = {t:i for i,t in enumerate(sorted(df))}
+        N = len(docs)
+        self.idf = {t: math.log((1+N)/(1+df[t]))+1 for t in self.vocab}
+
+    def transform(self,docs):
+        rows = []
+
+        for d in docs:
+            toks = d.lower().split()
+            tf = Counter(toks)
+            vec = {}
+            for t,c in tf.items():
+                if t in self.vocab:
+                    i = self.vocab[t]
+                    vec[i] = (c/len(toks))*self.idf[t]
+            rows.append(vec)
+        return rows
+    
+    def cosine_sparse(a,b):
+        keys = a.keys() & b.keys()
+        num = sum(a[i]*b[i] for i in keys)
+        na = math.sqrt(sum(v*v for v in a.values()))
+        nb = math.sqrt(sum(v*v for v in b.values()))
+        return 0 if na==0 or nb==0 else num/(na*nb)
+
+    
+    def topk_sparse(query_vec,doc_vecs, k=5):
+        sc = [(i, cosine_sparse(query_vec,v)) for i, v in enumerate(doc_vecs)]
+        sc.sort(key=lambda x:x[1],reverse=True)
+        return sc[:k]
 
 
 
