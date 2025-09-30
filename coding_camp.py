2dc32b536095e3ff31802d33ad603762694b8a86
FLotfiGit <flotfi@clemson.edu>
2025-09-30 13:25:10 -0400

    add jaccard_similarity function for documents

--- coding_camp.py
+++ coding_camp.py
@@ -37,6 +37,14 @@ class TFIDF:
         self.fit(docs)
         return self.transform(docs)
 
+def jaccard_similarity(doc1, doc2):
+    """Compute Jaccard similarity between two documents (strings)."""
+    set1 = set(doc1.lower().split())
+    set2 = set(doc2.lower().split())
+    intersection = set1 & set2
+    union = set1 | set2
+    return len(intersection) / len(union) if union else 0.0
+
 
 
 
